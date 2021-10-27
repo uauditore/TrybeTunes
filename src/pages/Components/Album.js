e92d84c2094fff0236fd81703124c48817c310b6
uauditore <fauditore2912@gmail.com>
2021-10-26 21:42:54 -0300

    finalizando por hoje, bem encaminhado

--- src/pages/Components/Album.js
+++ src/pages/Components/Album.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Header from 'prop-types';
+import Header from './Header';
 import getMusics from '../../services/musicsAPI';
 import MusicCard from './MusicCard';
 
@@ -25,6 +25,37 @@ class Album extends React.Component {
     const { params } = match;
     const { id } = params;
 
-    
+    getMusics(id).then((response) => this.setState({
+      album: response[0],
+      songs: response.slice(1),
+    }));
+  }
+
+  render() {
+    const { album, songs } = this.state;
+    const { artistName, collectionName } = album;
+    return (
+      <div data-testid="page-album">
+        <Header />
+        <h3 data-testid="artist-name">
+          {artistName}
+        </h3>
+        <p data-testid="album-name">
+          {`${collectionName} by ${artistName}`}
+        </p>
+        {songs.map((track) => (
+          <MusicCard key={ track.trackId } song={ track } />
+        ))}
+      </div>
+    );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
+export default Album;
